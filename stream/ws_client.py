6f7ea9a5b23556d38df7b97941c63930e5c150c6
sujeeth961991 <suji961991@gmail.com>
2018-03-06 21:11:13 +0530

    removing getting query params from kwargs and just iterating over kwargs

--- stream/ws_client.py
+++ stream/ws_client.py
@@ -235,7 +235,7 @@ def websocket_call(configuration, *args, **kwargs):
 
     # Expand command parameter list to indivitual command params
     query_params = []
-    for key, value in kwargs.get("query_params", {}).items():
+    for key, value in kwargs.items():
         if key == 'command' and isinstance(value, list):
             for command in value:
                 query_params.append((key, command))
