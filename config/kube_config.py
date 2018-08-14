bf80d26c8f13683bbcca6121b8ffe92b0016a033
Marcus Watkins <mwatkins@mitre.org>
2018-08-14 20:37:48 +0000

    Fix defaults not being honored

--- config/kube_config.py
+++ config/kube_config.py
@@ -514,7 +514,7 @@ def new_client_from_config(
     """Loads configuration the same as load_kube_config but returns an ApiClient
     to be used with any API object. This will allow the caller to concurrently
     talk with multiple clusters."""
-    client_config = type.__call__(Configuration)
+    client_config = Configuration()
     load_kube_config(config_file=config_file, context=context,
                      client_configuration=client_config,
                      persist_config=persist_config)
