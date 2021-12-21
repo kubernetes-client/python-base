04f65ee848eeb5269f9f6c78fdcbca5cd1190a60
Dipto Chakrabarty <45638240+DiptoChakrabarty@users.noreply.github.com>
2021-12-22 00:00:20 +0530

    remove basiconfig logger

--- config/__init__.py
+++ config/__init__.py
@@ -21,7 +21,6 @@ from .kube_config import (KUBE_CONFIG_DEFAULT_LOCATION,
                           list_kube_config_contexts, load_kube_config,
                           load_kube_config_from_dict, new_client_from_config, new_client_from_config_dict)
 
-logging.basicConfig(level=logging.INFO)
 
 def load_config(**kwargs):
     """
